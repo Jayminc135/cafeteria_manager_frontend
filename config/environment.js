ea2f946bf5f21cd320517d8a713f7e6439a997c0
Jaymin Chauhan <jaymin.chauhan@freshworks.com>
2023-08-16 22:11:52 +0530

    backend url added

--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.APP.URL = 'https://cafeteria-manager-backend.onrender.com/'
   }
 
   return ENV;
